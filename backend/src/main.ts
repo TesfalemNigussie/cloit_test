a1847ae582c3b770a4de3f2b0e860d2fab36a0a1
Tesfalem Nigussie <tesfalem.nigussie.h@gmail.com>
2025-02-18 10:19:55 +0300

    port number updated

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,14 +3,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  
+
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors();
 
-  await app.listen(3001);
+  await app.listen(54321);
 }
 
 bootstrap();
